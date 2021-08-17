88e3af38895989a801174ec6b91a413992f2d6cf
Aromdon <aromdon135@gmail.com>
2021-08-17 16:00:57 +0700

    aplikasi library

---
+++ app/src/main/java/com/romdon/mylibrary/MainActivity.java
@@ -0,0 +1,54 @@
+package com.romdon.mylibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnStore, btnGetall;
+    private EditText etname;
+    private DataBaseHelper dataBaseHelper;
+    private TextView tvnames;
+    private ArrayList<String> arrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dataBaseHelper = new DataBaseHelper(this);
+        tvnames = (TextView) findViewById(R.id.tvnames);
+
+        btnStore = (Button) findViewById(R.id.btnstore);
+        btnGetall = (Button) findViewById(R.id.btnget);
+        etname = (EditText) findViewById(R.id.etname);
+
+        btnStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataBaseHelper.addStudentDetail(etname.getText().toString());
+                etname.setText("");
+                Toast.makeText(MainActivity.this, "Stored Succesfully!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnGetall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arrayList=dataBaseHelper.getAllStudentList();
+                tvnames.setText("");
+                for (int i= 0;i <arrayList.size();i++){
+                    tvnames.setText(tvnames.getText().toString()+","+arrayList.get(i) );
+                }
+            }
+        });
+    }
+}
